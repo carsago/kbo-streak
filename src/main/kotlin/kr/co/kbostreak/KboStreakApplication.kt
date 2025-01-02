e837d415d3ddacc08280d309d56f76f326271e79
carsago <ohseo8813@gmail.com>
2025-01-02 20:20:05 +0900

    init

---
+++ src/main/kotlin/kr/co/kbostreak/KboStreakApplication.kt
@@ -0,0 +1,11 @@
+package kr.co.kbostreak
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KboStreakApplication
+
+fun main(args: Array<String>) {
+  runApplication<KboStreakApplication>(*args)
+}
